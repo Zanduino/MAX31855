9799dd6017a0bb7dcfdf3f37dfc1c9466575e03b
Doxygen CI <travis@travis-ci.org>
2020-11-30 13:41:06 +0000

    Deploy code docs to GitHub Pages Travis build: 26
    Commit: 2ab218d297a544f4ffad427b822d61af73d32181

--- html/class_m_a_x31855___class.js
+++ html/class_m_a_x31855___class.js
@@ -2,9 +2,9 @@ var class_m_a_x31855___class =
 [
     [ "MAX31855_Class", "class_m_a_x31855___class.html#acee758be3e21549555f3dffa10bd6769", null ],
     [ "~MAX31855_Class", "class_m_a_x31855___class.html#ac88c27ebf599985985776900d38a19ec", null ],
-    [ "begin", "class_m_a_x31855___class.html#a22b1085cb54de08c4fba747699669f77", null ],
-    [ "begin", "class_m_a_x31855___class.html#a98a31ba0837a466cba7b7daffd9e311a", null ],
-    [ "fault", "class_m_a_x31855___class.html#ac0d85776414cc97925dd581caedffe76", null ],
+    [ "begin", "class_m_a_x31855___class.html#a7183ab722475f6fe6a4fef5a83f3ce16", null ],
+    [ "begin", "class_m_a_x31855___class.html#af6b8b2edec0f8560dc4e90704983cd8a", null ],
+    [ "fault", "class_m_a_x31855___class.html#a4bc8e80d6857c8882035936dc2836ce4", null ],
     [ "readAmbient", "class_m_a_x31855___class.html#a3b06d075b79b4793cc15ed0bdeac8e81", null ],
     [ "readProbe", "class_m_a_x31855___class.html#a86fec1388c9a486f57cdb0172fc1ab1d", null ]
 ];
