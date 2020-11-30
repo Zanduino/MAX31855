a1d55bc446f9584c6dc4c46d5572f52bf076c7fb
Doxygen CI <travis@travis-ci.org>
2020-11-30 13:22:32 +0000

    Deploy code docs to GitHub Pages Travis build: 24
    Commit: 81e474397f3fb3dcb35d3d81c48d0d56036d38f0

--- html/search/functions_1.js
+++ html/search/functions_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['fault',['fault',['../class_m_a_x31855___class.html#a4bc8e80d6857c8882035936dc2836ce4',1,'MAX31855_Class']]]
+  ['fault',['fault',['../class_m_a_x31855___class.html#ac0d85776414cc97925dd581caedffe76',1,'MAX31855_Class']]]
 ];
