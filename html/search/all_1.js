4fcd64e863a385ba652da584a1d6988f216f25db
Doxygen CI <travis@travis-ci.org>
2020-11-30 08:43:33 +0000

    Deploy code docs to GitHub Pages Travis build: 21
    Commit: 933eb10a327f14fde207d071c343941c4a5a515b

--- html/search/all_1.js
+++ html/search/all_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['begin',['begin',['../class_m_a_x31855___class.html#a22b1085cb54de08c4fba747699669f77',1,'MAX31855_Class::begin(const uint8_t cs, const bool reverse=false)'],['../class_m_a_x31855___class.html#a98a31ba0837a466cba7b7daffd9e311a',1,'MAX31855_Class::begin(const uint8_t cs, const uint8_t miso, const uint8_t sck, const bool reverse=false)']]]
+  ['begin',['begin',['../class_m_a_x31855___class.html#a7183ab722475f6fe6a4fef5a83f3ce16',1,'MAX31855_Class::begin(const uint8_t chipSelect, const bool reverse=false)'],['../class_m_a_x31855___class.html#af6b8b2edec0f8560dc4e90704983cd8a',1,'MAX31855_Class::begin(const uint8_t chipSelect, const uint8_t miso, const uint8_t sck, const bool reverse=false)']]]
 ];
